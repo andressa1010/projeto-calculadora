2610a28317c12a4342e805ce15ce399b30fe4aa2
andressa1010 <andressa.ns10@gmail.com>
2024-08-21 20:35:03 -0300

    calculadora

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ function limparTela(){
     document.querySelector('.display').value= ''
 }
 
+
 function calcular(){
-    const valorInput=document.querySelector('.display').value  
-     document.querySelector('.display').value= eval(valorInput)
+    const valorInput = document.querySelector('.display').value;
+    const valorCalculado = valorInput.replace(/x/g, '*').replace(/÷/g, '/');
+    document.querySelector('.display').value = eval(valorCalculado);
 }
 
 function inverte(){
     const valorInput=document.querySelector('.display').value  
     document.querySelector('.display').value= valorInput * -1
 
+}
+
+function apagarUltimo(){
+    const display = document.querySelector('.display');
+    display.value = display.value.slice(0, -1);
 }
